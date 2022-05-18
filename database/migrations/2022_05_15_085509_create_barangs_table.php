4d1301eea6e1292894272bc6b5e280ebc37b55dc
blitzdps <mr.devil90blitz@gmail.com>
2022-05-19 01:53:06 +0800

    tambah database dimas

---
+++ database/migrations/2022_05_15_085509_create_barangs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->primary('id_barang');
+            $table->string('jenis_barang');
+            $table->string('harga');
+            $table->string('keterangan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+}
