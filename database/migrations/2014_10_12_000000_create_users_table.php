d51f707f9af0c58670bd2b71ef86382903810ddb
blitzdps <mr.devil90blitz@gmail.com>
2022-05-23 02:20:21 +0800

    tambah migration user dimas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->primary('id_user');
+            $table->string('nama_user');
+            $table->foreignId('id_level');
+            $table->string('alamat');
+            $table->string('hp');
+            $table->foreignId('id_kota');
+            $table->foreignId('id_kecamatan');
         });
     }
 
