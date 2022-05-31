802b11cef0b9894fcecb47baf28e0115815920e8
blitzdps <mr.devil90blitz@gmail.com>
2022-05-31 21:41:13 +0800

    update untuk menu data destinasi kota

--- resources/views/dashboard/datadestinasi/kota.blade.php
+++ resources/views/dashboard/datadestinasi/kota.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.layout')
 @section('content')
-        
 
 <h2 class="intro-y text-lg font-medium mt-10">
     Data Kota
 </h2>
+
 <div class="grid grid-cols-12 gap-6 mt-5">
     <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
-        <a  href="javascript:;" data-toggle="modal" data-target="#add-item-modal" class="btn btn-primary shadow-md mr-2">Tambah Kota</a>
+        <a  data-toggle="modal" data-target="#add-item-modal" class="btn btn-primary shadow-md mr-2">Tambah Kota</a>
         <div class="dropdown">
             <button class="dropdown-toggle btn px-2 box text-gray-700 dark:text-gray-300" aria-expanded="false">
                 <span class="w-5 h-5 flex items-center justify-center"> <i class="w-4 h-4" data-feather="plus"></i> </span>
             </button>
-            <div class="dropdown-menu w-20">
+            <div class="dropdown-menu w-40">
                 <div class="dropdown-menu__content box dark:bg-dark-1 p-2">
-                    <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </a>
-                    <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"> <i data-feather="file-text" class="w-4 h-4 mr-2"></i> Export to Excel </a>
-                    <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"> <i data-feather="file-text" class="w-4 h-4 mr-2"></i> Export to PDF </a>
+                    <a href="" class="flex items-center p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </a>
+                    <a href="" class="flex items-center p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"> <i data-feather="file-text" class="w-4 h-4 mr-2"></i> Export to Excel </a>
+                    <a href="" class="flex items-center p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"> <i data-feather="file-text" class="w-4 h-4 mr-2"></i> Export to PDF </a>
                 </div>
             </div>
         </div>
@@ -28,6 +28,7 @@
             </div>
         </div>
     </div>
+    
     <!-- BEGIN: Data List -->
     <div class="intro-y col-span-12 overflow-auto lg:overflow-visible">
         @if ($errors->any())
@@ -36,7 +37,7 @@
     @endforeach <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"> <i data-feather="x" class="w-4 h-4"></i> </button> </div>
        
     @endif
-        <table class="table table-report -mt-2">
+        <table class="table table-report -mt-2" id="myTabel">
             <thead>
                 <tr>
                     <th class="whitespace-nowrap">No</th>
@@ -46,35 +47,9 @@
                     <th class="text-center whitespace-nowrap">ACTIONS</th>
                 </tr>
             </thead>
-            <tbody>
-                @php
-                $no = 1;
-                @endphp
-                @foreach ($kotas as $item)
-                <tr class="intro-x">
-                    <td class="w-40">
-                        <a href="" class="font-medium whitespace-nowrap">{{ $no++ }}</a> 
-                    </td>
-                    <td>
-                        <a href="" class="font-medium whitespace-nowrap">{{ $item->nama_kota }}</a> 
-                    </td>
-                    <td class="w-40 text-center">
-                        {{ $item->kode_kota }}
-                    </td>
-                    <td class="w-40 text-center">
-                        {{ $item->keterangan }}
-                    </td>
-                    <td class="table-report__action w-56">
-                        <div class="flex justify-center items-center">
-                            <a class="flex items-center mr-3" href="javascript:;"> <i data-feather="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
-                            <a class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> <i data-feather="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
-                        </div>
-                    </td>
-                </tr>
-                @endforeach
-
-            </tbody>
+            <tbody id="showData"></tbody>
         </table>
+        
     </div>
     <!-- END: Data List -->
     <!-- BEGIN: Pagination -->
@@ -87,8 +62,8 @@
                 <a class="pagination__link" href=""> <i class="w-4 h-4" data-feather="chevron-left"></i> </a>
             </li>
             <li> <a class="pagination__link" href="">...</a> </li>
-            <li> <a class="pagination__link" href="">1</a> </li>
-            <li> <a class="pagination__link pagination__link--active" href="">2</a> </li>
+            <li> <a class="pagination__link pagination__link--active" href="">1</a> </li>
+            <li> <a class="pagination__link " href="">2</a> </li>
             <li> <a class="pagination__link" href="">3</a> </li>
             <li> <a class="pagination__link" href="">...</a> </li>
             <li>
@@ -107,29 +82,7 @@
     </div>
     <!-- END: Pagination -->
 </div>
-<!-- BEGIN: Delete Confirmation Modal -->
-<div id="delete-confirmation-modal" class="modal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-body p-0">
-                <div class="p-5 text-center">
-                    <i data-feather="x-circle" class="w-16 h-16 text-theme-6 mx-auto mt-3"></i> 
-                    <div class="text-3xl mt-5">Are you sure?</div>
-                    <div class="text-gray-600 mt-2">
-                        Do you really want to delete these records? 
-                        <br>
-                        This process cannot be undone.
-                    </div>
-                </div>
-                <div class="px-5 pb-8 text-center">
-                    <button type="button" data-dismiss="modal" class="btn btn-outline-secondary w-24 mr-1">Cancel</button>
-                    <button type="button" class="btn btn-danger w-24">Delete</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- END: Delete Confirmation Modal -->
+
 
  <!-- BEGIN: Add Item Modal -->
  <div id="add-item-modal" class="modal" tabindex="-1" aria-hidden="true">
@@ -141,9 +94,7 @@
                 </h2>
             </div>
 
-            
-
-            <form method="post" action="{{ route('kota.store') }}">
+            <form method="post" id="form_tambah" onsubmit="return false;" enctype="multipart/form-data">
                 @csrf
                 <div class="modal-body grid grid-cols-12 gap-4 gap-y-3">
                     <div class="col-span-12">
@@ -152,26 +103,185 @@
                     </div>
                     <div class="col-span-12">
                         <label for="kode_kota" class="form-label">Kode Kota</label>
-                        <textarea id="kode_kota" class="form-control w-full mt-2" name="kode_kota" placeholder="kode kota"></textarea>
+                        <input type="text" id="kode_kota" name="kode_kota" class="form-control w-full mt-2" placeholder="Kode Kota">
                     </div>
                     <div class="col-span-12">
                         <label for="keterangan" class="form-label">Keterangan</label>
                         <textarea id="keterangan" class="form-control w-full mt-2" name="keterangan" placeholder="keterangan"></textarea>
                     </div>
-                   
                 
                 </div>
                 <div class="modal-footer text-right">
                     <button data-dismiss="modal" class="btn btn-outline-secondary w-24 mr-1">Batal</button>
-                    <button type="submit" class="btn btn-primary w-24">Tambah</button>
+                    <button type="submit" data-dismiss="modal" class="btn btn-primary w-24">Tambah</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
 <!-- END: Add Item Modal -->
+
+ <!-- BEGIN: Modal Content -->
+ <div id="success-saved" class="modal " tabindex="-1" aria-hidden="true" >
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body p-0">
+                <div class="p-5 text-center"> <i data-feather="check-circle" class="w-16 h-16 text-theme-9 mx-auto mt-3"></i>
+                    <div class="text-3xl mt-5">Berhasil Tersimpan!</div>
+                    <div class="text-gray-600 mt-2">Data Anda tersimpan!</div>
+                </div>
+                <div class="px-5 pb-8 text-center"> <button type="button" data-dismiss="modal" class="btn btn-primary w-24">Ok</button> </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- END: Modal Content -->
+
+{{-- <div class="overflow-y-auto flex justify-center items-center" style="height: 100vh;position: fixed;z-index: 10000000;background: rgba(0,0,0,.6509803921568628);margin-top: 0px;margin-left: 0px;padding-left: 0px;z-index: 10000;width: 100vw;top: 0;left: 0;"> 
+    <div class="flex justify-center items-center">
+                        
+        <svg width="20" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8">
+            <defs>
+                <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
+                    <stop stop-color="rgb(45, 55, 72)" stop-opacity="0" offset="0%"></stop>
+                    <stop stop-color="rgb(45, 55, 72)" stop-opacity=".631" offset="63.146%"></stop>
+                    <stop stop-color="rgb(45, 55, 72)" offset="100%"></stop>
+                </linearGradient>
+            </defs>
+            <g fill="none" fill-rule="evenodd">
+                <g transform="translate(1 1)">
+                    <path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" stroke-width="3">
+                        <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"></animateTransform>
+                    </path>
+                    <circle fill="rgb(45, 55, 72)" cx="36" cy="18" r="1">
+                        <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"></animateTransform>
+                    </circle>
+                </g>
+            </g>
+        </svg>
+        </div>
+</div> --}}
+ 
+
+@stop
+@section('script')
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.17/dist/sweetalert2.all.min.js"></script>
+<script>
     
+    function showData(){
+        $.ajax({
+            url:"{{URL::to('datadestinasi/kota-list')}}",
+            type:"GET",
+            data:'data',
+            // beforeSend:function(){
+            //     $("#showData").after().empty().html(`<tr>
+            //         <td colspan="5">
+                    
+            //         <div role="alert" class="w-full alert alert-secondary show flex items-center mb-2"> <svg width="20" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8">
+            //                                 <defs>
+            //                                     <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
+            //                                         <stop stop-color="rgb(45, 55, 72)" stop-opacity="0" offset="0%"></stop>
+            //                                         <stop stop-color="rgb(45, 55, 72)" stop-opacity=".631" offset="63.146%"></stop>
+            //                                         <stop stop-color="rgb(45, 55, 72)" offset="100%"></stop>
+            //                                     </linearGradient>
+            //                                 </defs>
+            //                                 <g fill="none" fill-rule="evenodd">
+            //                                     <g transform="translate(1 1)">
+            //                                         <path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" stroke-width="3">
+            //                                             <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"></animateTransform>
+            //                                         </path>
+            //                                         <circle fill="rgb(45, 55, 72)" cx="36" cy="18" r="1">
+            //                                             <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"></animateTransform>
+            //                                         </circle>
+            //                                     </g>
+            //                                 </g>
+            //                             </svg>  &nbsp;&nbsp;Memuat Data </div>
+            //         </td></tr>`
+            //     );
+                
+            // },
+            success:function(result){
+                $("#showData").empty().html(result);
+            }
+        })
+    }
+
+    $(document).on("submit","#form_tambah",function(e){
+        var data = new FormData(this);
         
-       
+        if($("#form_tambah")[0].checkValidity()) {
+            //updateAllMessageForms();
+            e.preventDefault();
+            $.ajax({
+                url         : "{{route('kota.store')}}",
+                type        : 'post',
+                data        : data,
+                dataType    : 'JSON',
+                contentType : false,
+                cache       : false,
+                processData : false,
+                success: function(data) {
+                    modal.show('#success-saved'); 
+                    showData();
+                }
+                        
+            });
+        }
+    });
+
+    $('#myTabel').on('click', '#btn-delete', function(e) {
+        var id = $(this).data('id');
+
+        const swalWithTailwindpButtons = Swal.mixin({
+            customClass: {
+                confirmButton: 'mr-1 btn btn-danger w-24 focus:outline-none',
+                cancelButton: 'btn btn-outline-secondary mr-3 w-24'
+            },
+            buttonsStyling: false
+        })
         
+        swalWithTailwindpButtons.fire({
+            title: 'Apa anda Yakin?',
+            text: "Data tidak dapat dikembalikan!",
+            icon: 'warning',
+            showCancelButton: true,
+            iconColor: '#d32929',
+            confirmButtonText: 'Hapus',
+            cancelButtonText: 'Batal',
+            reverseButtons: true
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $.ajax({
+                        url         : "{{URL::to('datadestinasi/kota')}}/"+id,
+                        type        : 'delete',
+                        contentType : false,
+                        cache       : false,
+                        processData : false,
+                        success: function(data) {
+                            Swal.fire({
+                                
+                                icon: 'success',
+                                title: 'Deleted!',
+                                text:data.pesan,
+                                showConfirmButton: false,
+                                iconColor: '#1c3faa',
+                                timer: 1500
+                                })
+                                
+                            showData();
+                        }
+                                
+                    });
+                }
+            })
+
+    });
+
+
+    $(document).ready(function(){        
+        showData(); 
+
+    });
+
+    
+</script>
 @stop
