8a55ccf23745ab6e296afc9c314753f4c8c2e014
ammar.w <ammarwaliyuddin@gmail.com>
2022-06-03 01:21:03 +0700

    Update no_resi

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Destinasi;
 use Illuminate\Http\Request;
 use App\Models\Transaksi;
+use App\Models\Kota;
 
 class TransaksiController extends Controller
 {
@@ -36,9 +38,22 @@ class TransaksiController extends Controller
 
             return $data;
         }else{
-           
-            // $show = Barang::create();
+            
+            //BEGIN :: Generator No Resi
+            $sixRandom = rand(100000,999999);
+            $destinasi=Destinasi::where('id_destinasi',$request->input('destinasi'))->first();
+            
+            $kota=Kota::where('id_kota',$destinasi->id_kota_origin)->first();
+            $threeRandom = rand(100,999);
+            $date = date('my');
+
+            $no_resi = $sixRandom.$kota->kode_kota.$threeRandom.$date;
+            
+            // END :: Generator No Resi
+
+
             $transaksi=new Transaksi();
+            $transaksi->no_resi = $no_resi;
             $transaksi->id_pengirim = $request->input('pengirim');
             $transaksi->id_penerima = $request->input('pengirim');
             $transaksi->id_destinasi = $request->input('pengirim');
@@ -58,29 +73,6 @@ class TransaksiController extends Controller
     }
 
     
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
